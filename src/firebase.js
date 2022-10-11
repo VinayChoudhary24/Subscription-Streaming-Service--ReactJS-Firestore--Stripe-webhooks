a55b26b9e923e323382542b3100c233619b5eb2d
VinayPsych <vinaychoudhary994@gmail.com>
2022-10-11 19:50:25 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBJ95aeCekS4OwJMEzu57IcH02ASWvmrM4",
+    authDomain: "subscription-streaming-service.firebaseapp.com",
+    projectId: "subscription-streaming-service",
+    storageBucket: "subscription-streaming-service.appspot.com",
+    messagingSenderId: "1078877034337",
+    appId: "1:1078877034337:web:de3f06e9a0e827d7f91720"
+  };
